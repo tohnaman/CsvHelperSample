07d71fbaa0920f42337f5d20e26241f78be67a1e
tohnaman <tohnaman.76@outlook.jp>
2018-12-12 23:20:29 +0900

    初回コミット

---
+++ src/CsvHelperSample/CsvItem.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.Text;
+
+namespace CsvHelperSample
+{
+    public class CsvItem
+    {
+        public Header Header { get; set; }
+        public List<Person> PersonList { get; set; }
+
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+            sb.Append($"{this.Header.Title} ");
+            foreach (var item in this.PersonList)
+            {
+                sb.Append($"{item.LastName}{item.FirstName} {item.Age}歳 ");
+            }
+            return sb.ToString();
+        }
+    }
+
+    public class Header
+    {
+        public string Title { get; set; }
+    }
+
+    public class Person
+    {
+        public string LastName { get; set; }
+        public string FirstName { get; set; }
+        public int Age { get; set; }
+    }
+}
---
+++ src/CsvHelperSample/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using CsvHelper;
+using CsvHelper.Configuration;
+
+namespace CsvHelperSample
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var list = ReadCsv();
+            foreach (var item in list)
+            {
+                Console.WriteLine(item.ToString());
+            }
+            Console.ReadKey();
+        }
+
+        private static List<CsvItem> ReadCsv()
+        {
+            using (var reader = new StreamReader(@".\sample.csv", Encoding.UTF8))
+            using (var csv = new CsvReader(reader))
+            {
+                csv.Configuration.HasHeaderRecord = true;
+                csv.Configuration.RegisterClassMap<CsvItemMap>();
+                return csv.GetRecords<CsvItem>().ToList();
+            }
+        }
+    }
+
+
+    public sealed class CsvItemMap : ClassMap<CsvItem>
+    {
+        public CsvItemMap()
+        {
+            Map(m => m.Header.Title).Index(0);
+            Map(m => m.PersonList).ConvertUsing(row =>
+            {
+                var list = new List<Person>();
+
+                var index = 1;
+                for (var i = 0; i < 3; i++)
+                {
+                    var person = new Person()
+                    {
+                        LastName = row.GetField(index++),
+                        FirstName = row.GetField(index++),
+                        Age = int.Parse(row.GetField(index++)),
+                    };
+                    list.Add(person);
+                }
+
+
+                return list;
+            });
+        }
+    }
+}
